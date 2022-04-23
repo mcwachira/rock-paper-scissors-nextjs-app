b5c3210b5ba317963f2b7ff83d32b695f45e0f05
mcwachira <23430963+mcwachira@users.noreply.github.com>
2022-04-24 02:16:39 +0300

    added the rules component and footer

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import classes from '../styles/Home.module.css'
 import{useState} from 'react'
 
+
+//importing our components
 import Header from '../components/Header/Header.component'
+import Rules from '../components/Rules/Rules.component'
 
 export default function Home() {
 
@@ -11,14 +14,27 @@ export default function Home() {
 //added the score state so as to change the value of our score
   const [score, setScore] = useState(0);
 
+  //open state will enable us to view the rules component when we click the rules button
+  const[open, setOpen] = useState(false)
+
   return (
-    <div>
+    <div >
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header Score={score} />
   
+  <footer className={classes.footer}>
+    <div className={classes.rules} onClick={() => setOpen(!open)}>
+      Rules
+    </div>
+  </footer>
+
+  <Rules
+  open={open}
+  setopen={() => setOpen(!open)}
+  />
     </div>
   )
 }
